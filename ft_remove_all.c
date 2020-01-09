70fc839a8190b93d95495b647a62b04b1ace9034
Marc LI <mli@e3r6p6.42.fr>
2020-01-09 14:10:31 +0100

    remove all ok

--- ft_remove_all.c
+++ ft_remove_all.c
@@ -8,22 +8,39 @@
 
 #include "cub3d.h"
 
+void	ft_remove_draw(t_win *draw)
+{
+	if (draw->mlx && draw->win)
+		mlx_destroy_window(draw->mlx, draw->win);
+	if (draw->mlx && draw->img)
+		mlx_destroy_image(draw->mlx, draw->img);
+	ft_free((void **)&draw);
+}
+
+void	ft_remove_parse(t_parsing *parse)
+{
+	int i;
+
+	i = 0;
+	while (i < parse->map_size)
+		ft_free((void **)(&parse->map[i++]));
+	ft_free((void **)(&parse->map));
+	ft_free((void **)&parse->txt_w);
+	ft_free((void **)&parse);
+}
+
 int		ft_remove_all(t_param *hub)
 {
 	if (hub != NULL)
 	{
 		if (hub->draw)
-			; // Destroy the MLX
-		free(hub->draw);
-		hub->draw = NULL;
-
-		free(hub->space);
-		hub->space = NULL;
-
-		free(hub->funct);
-		hub->funct = NULL;
+			ft_remove_draw(hub->draw);
+		if (hub->parse)
+			ft_remove_parse(hub->parse);
+		ft_free((void **)&hub->space);
+		ft_free((void **)&hub->funct);
 	}
-	free(hub);
+	ft_free((void **)&hub);
 	write(1, "Process stoped.\n", 16);
 	return (1);
 }
