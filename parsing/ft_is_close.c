f83362156005deeec6693f55a38aca3d525f7b43
Marc LI <mli@e3r6p6.42.fr>
2020-03-06 13:04:33 +0100

    Borders are closed

--- parsing/ft_is_close.c
+++ parsing/ft_is_close.c
@@ -6,30 +6,57 @@
 /*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/08 16:48:28 by mli               #+#    #+#             */
-/*   Updated: 2020/03/06 09:35:07 by mli              ###   ########.fr       */
+/*   Updated: 2020/03/06 12:59:59 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-int		ft_is_close(t_hub *hub, int **map, t_limit *border_x, int max_y)
+int		ft_iswallspace(char c)
+{
+	if (c == 1 || c == ' ')
+		return (1);
+	return (0);
+}
+
+int		ft_iswalkable(char c)
+{
+	if (ft_isposition(c) || c == 0 || c == 2)
+		return (1);
+	return (0);
+}
+
+int		ft_h_border(t_hub *hub, int **map, int row)
 {
 	int i;
-	int max_x;
-	int prev;
 
-	(void)hub;
+	i = -1;
+	while (++i < hub->env->width[row].border[1])
+		if (!ft_iswallspace(map[row][i]))
+			return (0);
+	return (1);
+}
+
+int		good_borders(t_hub *hub, int **map, t_limit *border_x, int full_height)
+{
+	int i;
+
+	i = -1;
+	while (++i < full_height)
+		if (!ft_iswallspace(map[i][0]))
+			return (0);
+	if (!ft_h_border(hub, map, 0) || !ft_h_border(hub, map, full_height - 1))
+		return (0);
 	i = 0;
-	while (++i < max_y)
-	{
-		prev = border_x[i - 1].border[1];
-		max_x = border_x[i].border[1];
-		while (prev < max_x)
-			if (map[i][--max_x - 1] != 1)
-				return (0);
-		while (prev >= max_x)
-			if (map[i - 1][--prev] != 1)
-				return (0);
-	}
+	while (++i < full_height)
+		if (!ft_iswallspace(map[i][border_x[i].border[1] - 1]))
+			return (0);
+	return (1);
+}
+
+int		ft_is_close(t_hub *hub, int **map, t_limit *border_x, int full_height)
+{
+	if (!good_borders(hub, map, border_x, full_height))
+		return (0);
 	return (1);
 }
