8e8b23fa09c1eed4919ed0b285f011bc6ea2bc3a
mli <mli@student.42.fr>
2020-03-18 14:58:13 +0100

    New check map closed

--- parsing/ft_is_close.c
+++ parsing/ft_is_close.c
@@ -6,7 +6,7 @@
 /*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/08 16:48:28 by mli               #+#    #+#             */
-/*   Updated: 2020/03/06 15:10:41 by mli              ###   ########.fr       */
+/*   Updated: 2020/03/18 14:50:55 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,40 +19,42 @@ int		ft_iswalkable(char c)
 	return (0);
 }
 
-int		ft_h_border(t_hub *hub, int **map, int row)
+int		is_outside_border(t_hub *hub, int i, int j)
 {
-	int i;
-
-	i = -1;
-	while (++i < hub->env->width[row].border[1])
-		if (!ft_iswallspace(map[row][i]))
-			return (0);
-	return (1);
+	if (i < 0 || j < 0 || j >= hub->env->full_height ||
+			i >= hub->env->width[j].border[1])
+		return (1);
+	return (0);
 }
 
 int		good_borders(t_hub *hub, int **map, t_limit *border_x, int full_height)
 {
 	int i;
+	int j;
 
 	i = -1;
 	while (++i < full_height)
-		if (!ft_iswallspace(map[i][0]))
+		if (!ft_iswallspace(map[i][0]) ||
+			!ft_iswallspace(map[i][border_x[i].border[1] - 1]))
 			return (0);
-	if (!ft_h_border(hub, map, 0) || !ft_h_border(hub, map, full_height - 1))
-		return (0);
-	i = 0;
-	while (++i < full_height)
-		if (!ft_iswallspace(map[i][border_x[i].border[1] - 1]))
-			return (0);
+	j = -1;
+	while (++j < full_height)
+	{
+		i = -1;
+		while (++i < border_x[j].border[1])
+			if (is_outside_border(hub, i, j - 1) ||
+				is_outside_border(hub, i, j + 1))
+				if (!ft_iswallspace(map[j][i]))
+					return (0);
+	}
 	return (1);
 }
 
-int		is_outside_border(t_hub *hub, int i, int j)
+int		ft_neighbour_ok(t_hub *hub, int **map, int x, int y)
 {
-	if (i <= 0 || j <= 0 || j >= hub->env->full_height ||
-			i >= hub->env->width[j].border[1])
-		return (1);
-	return (0);
+	if (!is_outside_border(hub, x, y) && map[y][x] == ' ')
+		return (0);
+	return (1);
 }
 
 int		ft_is_close(t_hub *hub, int **map, t_limit *border_x, int full_height)
@@ -68,16 +70,15 @@ int		ft_is_close(t_hub *hub, int **map, t_limit *border_x, int full_height)
 		i = -1;
 		while (++i < border_x[j].border[1])
 			if (ft_iswalkable(map[j][i]))
-			{
-				if (!is_outside_border(hub, i, j - 1) && map[j - 1][i] == ' ')
+				if (!ft_neighbour_ok(hub, map, i, j - 1) ||
+					!ft_neighbour_ok(hub, map, i, j + 1) ||
+					!ft_neighbour_ok(hub, map, i - 1, j) ||
+					!ft_neighbour_ok(hub, map, i + 1, j) ||
+					!ft_neighbour_ok(hub, map, i - 1, j - 1) ||
+					!ft_neighbour_ok(hub, map, i + 1, j - 1) ||
+					!ft_neighbour_ok(hub, map, i - 1, j + 1) ||
+					!ft_neighbour_ok(hub, map, i + 1, j + 1))
 					return (0);
-				if (!is_outside_border(hub, i, j + 1) && map[j + 1][i] == ' ')
-					return (0);
-				if (!is_outside_border(hub, i - 1, j) && map[j][i - 1] == ' ')
-					return (0);
-				if (!is_outside_border(hub, i + 1, j) && map[j][i + 1] == ' ')
-					return (0);
-			}
 	}
 	return (1);
 }
